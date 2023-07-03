0283008086a088b56d749d80857c4287a7e65274
shiranezu <aweleoluonye@gmail.com>
2023-07-03 12:42:51 +0100

    h

---
+++ assessment17.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self, name, age, score):
+        self.name = name(str)
+        self.age = age(int)
+        self.score = score(int)
+
+with open('name.txt', 'w+') as file:
+
+
+    
